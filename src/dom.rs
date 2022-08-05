5da9393a44bc799cfa6ae8031bfb62025892aeda
Petter Rasmussen <petter@hask.no>
2022-08-05 17:28:48 +0300

    Add CaptureType enum

--- src/dom.rs
+++ src/dom.rs
@@ -1,8 +1,15 @@
-use serde_json::json;
 use std::fmt;
 
-// TODO: create enum?
-pub const VALUE_FROM_ID: &'static str = "VALUE_FROM_ID";
+#[derive(Clone, serde::Deserialize, serde::Serialize)]
+#[serde(tag = "type", content = "config")]
+#[serde(rename_all = "camelCase")]
+pub enum CaptureType {
+    #[serde(rename_all = "camelCase")]
+    ValueFromElement {
+        element_id: DomId,
+    },
+    WindowSize,
+}
 
 #[derive(Clone, serde::Deserialize, serde::Serialize)]
 pub struct Value(serde_json::Value);
@@ -16,7 +23,13 @@ impl Value {
     }
 
     pub fn from_id(id: &DomId) -> Value {
-        Value(json!(format!("{}:{}", VALUE_FROM_ID, id)))
+        to_value(CaptureType::ValueFromElement {
+            element_id: id.clone(),
+        })
+    }
+
+    pub fn window_size() -> Value {
+        to_value(CaptureType::WindowSize)
     }
 }
 
@@ -276,5 +289,26 @@ impl From<&str> for DomId {
 impl fmt::Display for DomId {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.0)
+    }
+}
+
+#[derive(Clone, serde::Serialize, serde::Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct WindowSize {
+    pub width: u32,
+    pub height: u32,
+}
+
+fn to_value<T>(value: T) -> Value
+where
+    T: serde::Serialize,
+{
+    match serde_json::to_value(value) {
+        Ok(json_value) => Value(json_value),
+
+        Err(err) => Value(serde_json::Value::String(format!(
+            "Failed to serialize value: {}",
+            err
+        ))),
     }
 }
