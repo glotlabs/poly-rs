ea2d85f61ce7d1c48557f3d219b84ae31b5468be
Petter Rasmussen <petter@hask.no>
2022-08-06 15:59:28 +0300

    Add duration to id

--- src/browser/interval.rs
+++ src/browser/interval.rs
@@ -1,18 +1,17 @@
-use crate::browser::dom_id::DomId;
 use crate::browser::queue_strategy::QueueStrategy;
 
 #[derive(Clone, serde::Serialize, serde::Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Interval<Msg> {
-    id: DomId,
+    id: String,
     duration: u64,
     msg: Msg,
     queue_strategy: QueueStrategy,
 }
 
-pub fn interval<Msg>(id: DomId, duration: u64, msg: Msg) -> Interval<Msg> {
+pub fn interval<Msg>(id: String, duration: u64, msg: Msg) -> Interval<Msg> {
     Interval {
-        id,
+        id: format!("{}-{}", id, duration),
         duration,
         msg,
         queue_strategy: QueueStrategy::DropOlder,
