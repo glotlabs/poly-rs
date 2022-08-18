bc14a5ae0e53868e983d3cbf0da01068077cda42
Petter Rasmussen <petter@hask.no>
2022-08-18 08:26:29 +0300

    Simplify interval api

--- polyester/src/browser/subscription/interval.rs
+++ polyester/src/browser/subscription/interval.rs
@@ -1,5 +1,7 @@
+use crate::browser::Effect;
 use crate::browser::Subscription;
 use crate::browser::SubscriptionMsg;
+use crate::browser::Value;
 use std::time::Duration;
 
 #[derive(Clone, serde::Serialize)]
@@ -12,11 +14,26 @@ pub struct Interval<Msg, CustomEffect> {
 
 pub fn interval<Msg, CustomEffect>(
     duration: Duration,
-    msg: SubscriptionMsg<Msg, CustomEffect>,
+    msg: Msg,
 ) -> Subscription<Msg, CustomEffect> {
     Subscription::Interval(Interval {
         id: format!("interval-{}", duration.as_millis()),
         duration: duration.as_millis(),
-        msg,
+        msg: SubscriptionMsg::pure(msg),
+    })
+}
+
+pub fn interval_effect<Msg, CustomEffect, ToMsg>(
+    duration: Duration,
+    to_msg: ToMsg,
+    effect: Effect<Msg, CustomEffect>,
+) -> Subscription<Msg, CustomEffect>
+where
+    ToMsg: Fn(Value) -> Msg,
+{
+    Subscription::Interval(Interval {
+        id: format!("interval-{}", duration.as_millis()),
+        duration: duration.as_millis(),
+        msg: SubscriptionMsg::effectful(to_msg, effect),
     })
 }
