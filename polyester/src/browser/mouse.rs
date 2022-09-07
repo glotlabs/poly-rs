8aebf33b5339fc91531e18ee399147621cf15f75
Petter Rasmussen <petter@hask.no>
2022-09-07 08:28:14 +0300

    Add all button types

--- polyester/src/browser/mouse.rs
+++ polyester/src/browser/mouse.rs
@@ -4,12 +4,20 @@ use std::fmt;
 #[serde(rename_all = "camelCase")]
 pub enum Button {
     Main,
+    Auxiliary,
+    Secondary,
+    Fourth,
+    Fifth,
 }
 
 impl fmt::Display for Button {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Button::Main => write!(f, "main"),
+            Button::Auxiliary => write!(f, "auxiliary"),
+            Button::Secondary => write!(f, "secondary"),
+            Button::Fourth => write!(f, "fourth"),
+            Button::Fifth => write!(f, "fifth"),
         }
     }
 }
