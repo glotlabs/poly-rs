8afc23d54c19114c9b2dca4d5b75b50be2f2922d
Petter Rasmussen <petter@hask.no>
2024-07-16 11:03:36 +0200

    Add success and failure msgs to clipboard effect

--- poly/src/browser/effect/clipboard.rs
+++ poly/src/browser/effect/clipboard.rs
@@ -4,11 +4,21 @@ use crate::browser::Effect;
 #[serde(tag = "type", content = "config")]
 #[serde(rename_all = "camelCase")]
 pub enum Clipboard {
-    WriteText { text: String },
+    WriteText {
+        text: String,
+        success_msg_name: String,
+        failure_msg_name: String,
+    },
 }
 
-pub fn write_text<Msg, AppEffect>(s: &str) -> Effect<Msg, AppEffect> {
+pub fn write_text<Msg, AppEffect>(
+    s: &str,
+    success_msg_name: &str,
+    failure_msg_name: &str,
+) -> Effect<Msg, AppEffect> {
     Effect::Clipboard(Clipboard::WriteText {
         text: s.to_string(),
+        success_msg_name: success_msg_name.to_string(),
+        failure_msg_name: failure_msg_name.to_string(),
     })
 }
