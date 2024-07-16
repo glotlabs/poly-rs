40145b754b01ead91fb598b0c621f6f56d37ad34
Petter Rasmussen <petter@hask.no>
2024-07-16 11:13:16 +0200

    Consolidate into result_msg_name

--- poly/src/browser/effect/clipboard.rs
+++ poly/src/browser/effect/clipboard.rs
@@ -6,19 +6,13 @@ use crate::browser::Effect;
 pub enum Clipboard {
     WriteText {
         text: String,
-        success_msg_name: String,
-        failure_msg_name: String,
+        result_msg_name: String,
     },
 }
 
-pub fn write_text<Msg, AppEffect>(
-    s: &str,
-    success_msg_name: &str,
-    failure_msg_name: &str,
-) -> Effect<Msg, AppEffect> {
+pub fn write_text<Msg, AppEffect>(s: &str, result_msg_name: &str) -> Effect<Msg, AppEffect> {
     Effect::Clipboard(Clipboard::WriteText {
         text: s.to_string(),
-        success_msg_name: success_msg_name.to_string(),
-        failure_msg_name: failure_msg_name.to_string(),
+        result_msg_name: result_msg_name.to_string(),
     })
 }
